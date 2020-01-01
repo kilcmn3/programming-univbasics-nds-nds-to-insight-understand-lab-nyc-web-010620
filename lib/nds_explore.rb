b52cde2c83129e05eba29b51d8499c442c58d028
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-01 21:26:51 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ puts nds
 end
 
 def print_first_directors_movie_titles
-    row_index = 0;
-    while row_index < directors_database.count do
+    # row_index = 0;
+    # while row_index < directors_database.count do
 
 end
